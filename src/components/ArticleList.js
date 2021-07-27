64f4e2586598bcc00d555b174c8849ce759473d7
imyofather79 <chunsum708@gmail.com>
2021-07-27 12:26:50 -0600

    commit tree fbbc18d02676df60a63f75d86023871823192d53

---
+++ src/components/ArticleList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Article from "./Article";
+
+
+function ArticleList({ posts }){
+    const array = posts.map((post) => {
+        return (
+            <Article
+                key={ post.id }
+                title={ post.title }
+                date={ post.date }
+                preview={ post.preview }
+                minutes={ post.minutes }
+            />
+        );
+        })
+
+
+return <main>{ array }</main>
+
+};
+
+export default ArticleList;
